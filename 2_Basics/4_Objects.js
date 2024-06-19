f9d287173ba5d6939fcf062b007e88e052d0441f
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-19 18:14:37 +0530

    Basics in JS

--- 2_Basics/4_Objects.js
+++ 2_Basics/4_Objects.js
@@ -8,3 +8,25 @@ const usr2 = {}; // non singelton
 // clog(usr1);
 // clog(usr2);
 
+const user = {};
+
+user.id = 122 ;
+user.name  = "Shardendu Mishra";
+user.isLogged = true;
+
+// clog(user);
+
+const NewUser = {
+    email : "shardendumishra01@google.com";
+    fullname : {
+        nickname : {
+            n_name = "Krishna",
+        },
+        actualname : {
+            firstname = "Shardendu",
+            lastname = "Mishra",
+        },
+    }
+}
+
+clog();
