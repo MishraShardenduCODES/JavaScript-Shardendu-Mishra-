b22efc2679bc7e14b9c385ff0f9cb0fbef5331ae
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-19 18:53:06 +0530

    Basics in JS

--- 2_Basics/4_Objects.js
+++ 2_Basics/4_Objects.js
@@ -17,16 +17,75 @@ user.isLogged = true;
 // clog(user);
 
 const NewUser = {
-    email : "shardendumishra01@google.com";
+    email : "shardendumishra01@google.com",
     fullname : {
         nickname : {
-            n_name = "Krishna",
+            n_name : "Krishna",
         },
         actualname : {
-            firstname = "Shardendu",
-            lastname = "Mishra",
+            firstname : "Shardendu",
+            lastname : "Mishra",
         },
     }
 }
 
-clog();
+// clog(NewUser.fullname.nickname.n_name);
+
+
+const obj1 = {1 : "a" , 2 : "b"};
+const obj2 = {3 : "c" , 3 : "d"};
+const obj3 = {4 : "e" , 5 : "f"};
+
+// spread method 90% we use this
+const obj4 = {...obj1,...obj2,...obj3};
+// clog(obj4);
+
+// treats all as different objects
+const obj5 = {obj1,obj2,obj3};
+// clog(obj5);
+
+// Object.assign(target,source)
+
+// The result is stored in obj1 and obj6 both
+// const obj6 = Object.assign(obj1,obj2,obj3);
+// clog(obj1);
+// clog(obj6);
+
+// The result is stored in obj6 and obj1 remains unchanged
+// const obj6 = Object.assign({},obj1,obj2,obj3);
+// clog(obj1);
+// clog(obj6);
+
+
+// passing objects in array
+const Users = [
+    {
+        id: 1,
+        email_id: "h@gmail.com",
+    },
+    {
+        id: 2,  
+        email_id: "h2@gmail.com",
+    },
+    {
+        id: 3,  
+        email_id: "h3@gmail.com", 
+    }
+];
+
+// console.log(Array.isArray(Users)); // true
+
+// console.log(Users[0]); // Output: { id: 1, email_id: "h@gmail.com" }
+// console.log(Users[1]); // Output: { id: 2, email_id: "h2@gmail.com" }
+// console.log(Users[2]); // Output: { id: 3, email_id: "h3@gmail.com" }
+
+// console.log(Users[0].email_id); // Output: "h@gmail.com"
+// console.log(Users[1].email_id); // Output: "h2@gmail.com"
+// console.log(Users[2].email_id); // Output: "h3@gmail.com"
+
+// console.log(Users);
+
+
+clog(Object.keys(Users));
+clog(Object.values(Users));
+clog(Object.entries(Users));
