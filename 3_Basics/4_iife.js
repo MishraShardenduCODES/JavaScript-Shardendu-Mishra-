04c39f11662c5a98f2ecc70282c003f40d21fd82
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-20 01:09:31 +0530

    Basics in JS

--- 3_Basics/4_iife.js
+++ 3_Basics/4_iife.js
@@ -3,3 +3,29 @@
 const clog = console.log;
 const nl = "\n";
 
+// (function chai(){
+//     console.log("Shardendu Mishra");
+// })();
+
+
+// (() => {
+//     console.log("Shardendu Mishra");
+// })();
+
+// ((/*parameter*/) => {
+//     console.log(`${/*parameter*/} malum h naaa`);
+// })(/*argument*/);
+
+
+((name) => {
+    console.log(`${name} malum h naaa`);
+})("KR Dollar Sign");
+
+
+// (() => {})();
+// this mf is a function man fuck this shit
+
+
+// ((name) => {
+//     console.log(`${name} is sleeping`);
+// })("Shardendu Mishra");
