b0d67f18a3b2152bf01d3b02b2c0da8ee82ced04
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-20 21:18:01 +0530

    Iteration in JS

---
+++ 5_Iterator.js/3_forEach.js
@@ -0,0 +1,47 @@
+"use strict"
+
+const clog = console.log;
+const nl = "\n";
+
+
+const coding = [1,2,3,4,5,6,7,8,9];
+// coding.forEach( function(val){
+//     clog(val);
+// });
+
+
+// coding.forEach( (val) => {
+//     clog(val);
+// });
+
+// coding.forEach( (val,idx,arr) => {
+//     clog(`The value at index of ${idx} in the [ ${arr} ] is ${val}`);
+// });
+
+const itm = [
+    {
+        LangName : "Python",
+        LangFile : "py",
+    },
+    {
+        LangName : "JavaScript",
+        LangFile : "js",
+    },
+    {
+        LangName : "Java",
+        LangFile : "jv",
+    },
+    {
+        LangName : "C++",
+        LangFile : "cpp",
+    },
+]
+
+
+// const values = itm.forEach( (val) => {
+//     clog(`The Langauge name is ${val.LangName} and the file name is ${val.LangFile}`);
+// });
+
+// clog(values);
+
+
