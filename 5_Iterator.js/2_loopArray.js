c998b52743369aa792231c4c3e07fcdea474853f
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-20 12:00:47 +0530

    Iteration in JS

---
+++ 5_Iterator.js/2_loopArray.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const clog = console.log;
+const nl = "\n";
+
+// ["","","",""]
+// [{},{},{},{}]
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+// for(const i of arr){
+//     clog(i);
+// }
+
+// Maps
+
+const m = new Map();
+m.set("India",91);
+m.set("U.S.A",111);
+m.set("India",91);
+m.set("India",92);
+m.set("Indian",91);
+
+clog(m);
