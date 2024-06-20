cec1f17cae7848229d89830d419a728c2e8fd4f7
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-21 00:07:27 +0530

    Document Object Model

---
+++ DOM/1_DOM/1_dom.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const clog = console.log ;
+const nl = "\n" ;
+
+/////////////////////////////////////////////////////////////////
+
+
+// Browser at Windows object
+// clog(window);
+
+// At the next level is document
+// clog(window.document);
+
+// we can also access by 
+// console.dir(document);
+
+// to get base URL of this page
+// clog(document.baseURI);
+
+// to get links 
+// clog(document.links);
+
+// to get a specific link
+// clog(document.links[2]);
+
+// to get the id of an element
+// document.getElementById('catlinks');
+
+// to get element using class
+// document.getElementsByClassName('catlinks');
+
+
+/////////////////////////////////////////////////////////////////
