094e5e28650b9ece3ed0f221985feeb54ecc0a9d
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-25 20:57:55 +0530

    Async in JS

--- 10_Promises/async_function.js
+++ 10_Promises/async_function.js
@@ -1,19 +1,55 @@
 "use strict"
 
-const PromiseFive = new Promise(function(resolve,reject){
-    setTimeout(function(){
+// const PromiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let NoError = true;
+//         if(NoError){
+//             resolve({User : "Shardendu Mishra",Pass : "2244"});
+//         }else{
+//             reject("Error : Request has Errors");
+//         }
+//     },1000);
+// })
+// async function ConsumePromiseFive(){
+//     try {
+//         const response = await PromiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async function ConsumePromiseFive(){
+//     const Response =  await PromiseFive;
+//     console.log(Response);
+//     console.log(Response.User);
+//     console.log(Response.Pass);
+// };
+
+// ConsumePromiseFive();
+
+
+const ExtractDataFromDataBase = new Promise((resolve, reject) => {
+    setTimeout(function() {
         let NoError = true;
-        if(NoError){
-            resolve({User : "Shardendu Mishra",Pass : "2244"});
-        }else{
-            reject("Error : Request has Errors");
+        if (NoError) {
+            resolve({ username: "Shardendu Mishra", age: 20 });
+        } else {
+            reject("The Request Has Been Rejected!");
         }
-    },1000);
-})
+    }, 1000);
+});
 
-async function ConsumePromiseFive(){
-    const Response =  await PromiseFive;
-    console.log(Response);
-};
+async function DataExtracted() {
+    try {
+        const databaseData = await ExtractDataFromDataBase;
+        console.log(databaseData);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(`There has been an error: ${error}`);
+    }
+}
 
-ConsumePromiseFive();
+DataExtracted();
