3b9eb6600b1841a6c29b03dc86116268b3f58ce6
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-27 00:52:09 +0530

    Closure in JS

---
+++ 11 Fun With JS/Closure/index.js
@@ -0,0 +1,39 @@
+// function Outer(){
+//     let UserName = "Shardendu Mishra"
+//     function Inner(){
+//         console.log("Inner", UserName);
+//     }
+//     function InnerTwo(){
+//         console.log("InnerTwo", UserName);
+//     }
+
+//     Inner()
+//     InnerTwo()
+// }
+
+// Outer()
+// console.log("TOO OUTER");
+
+
+// document.querySelector(".orange").addEventListener('click',function(){
+//     document.body.style.backgroundColor = 'orange';
+// });
+// document.querySelector(".green").addEventListener('click',function(){
+//     document.body.style.backgroundColor = 'green';
+// });
+
+
+// What if there are 100 colors and u have to make button
+// function ChangeColor(color){
+//     return function(){
+//         document.body.style.backgroundColor = color;
+//     }
+// }
+
+
+// document.querySelector('.orange').addEventListener('click', ChangeColor('orange'));
+// document.querySelector('.green').addEventListener('click', ChangeColor('green'));
+// document.querySelector('.blue').addEventListener('click', ChangeColor('blue'));
+// document.querySelector('.pink').addEventListener('click', ChangeColor('pink'));
+
+
