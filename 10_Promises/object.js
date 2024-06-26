9a4295cb7c75e34c95f0323607d40c26c82cc841
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-26 10:37:17 +0530

    Objects and Prototypes in JS

---
+++ 10_Promises/object.js
@@ -0,0 +1,42 @@
+"use strict"
+
+// function Multiply5(num){
+//     return num*5;
+// }
+
+// Multiply5.power = 2;
+
+// console.log(Multiply5(5));
+// console.log(Multiply5.power);
+
+// this will return {}
+// console.log(Multiply5.prototype)
+
+// function CreateUser(name, score) {
+//     this.name = name;
+//     this.score = score;
+// };
+
+// CreateUser.prototype.increment = function() {
+//     this.score += 1;
+// };
+
+// const User1 = new CreateUser("Shardendu Mishra", 21);
+// const User2 = new CreateUser("Shivika Mishraaa", 21);
+
+// console.log(User1); 
+// console.log(User2); 
+
+// User1.increment();
+// console.log(User1.score); 
+
+
+// Define a method on String.prototype instead
+// Object.prototype.trueLength = function() {
+//     return this.trim().length;
+// };
+
+// let a = '    i am   ';
+// console.log(a.trueLength()); 
+
+// console.log('    i am shardendu mishra    '.trueLength()); 
